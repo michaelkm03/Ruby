4f6abf086d554eb3a95aebc5c3d6149417371be4
Michael Montgomery <michael.m@victorious.com>
2017-06-22 11:26:31 -0700

    initial commit, fastlane

--- convert.rb
+++ convert.rb
@@ -1,16 +1,19 @@
 require 'reverse_markdown'
 require 'FileUtils'
 
-html_dir = Dir.chdir("/Users/michaelmontgomery/Desktop/html")
+Dir.chdir("/Users/michaelmontgomery/Desktop/html")
 txt_dir = "/Users/michaelmontgomery/Desktop/txt"
-all_text_files = Dir.glob "*.html"
+all_html = Dir.glob "*.html"
 
 Dir.foreach(Dir.pwd) {|x|
+	puts Dir.pwd
+	puts x
 	next if x == '.' or x == '..' or x =='.DS_Store' or x == 'attachments' or x == 'styles' or x =='images'
 	file = File.open("/Users/michaelmontgomery/Desktop/html/#{x}", 'r')
 	data = file.read
-
 	result = ReverseMarkdown.convert data
 
-	destination = File.new("/Users/michaelmontgomery/Desktop/txt/#{x}.txt", "w")
+	# Create a new file and write to it  
+	f = File.new('/Users/michaelmontgomery/Desktop/txt/#{x}', 'w+')
+	f.puts result
 }
